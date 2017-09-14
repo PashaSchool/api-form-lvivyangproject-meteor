a3a5b97311ac5b71647a7f7b425a75e2edb20fb2
Pasha School <pasha.school888@gmail.com>
2017-09-14 22:04:26 +0300

    add eslint and scss package

--- client/main.js
+++ client/main.js
@@ -1,38 +1,50 @@
 import {Meteor} from 'meteor/meteor'
 import {Branch} from '../imports/api/branch.js'
 import { Tracker } from 'meteor/tracker'
+import React from 'react'
+import {render} from 'react-dom'
 
-import axios from 'axios'
+//components
+import App from '../imports/ui/App'
 
-const API_URL = 'https://lviv-it-yangproject-api.herokuapp.com';
-const LOCAL_URL = 'http://localhost:3000';
 
 Meteor.startup(() => {
-	document.getElementById('btn').addEventListener('click', function() {
-		axios.get(API_URL + '/api/branches').then(res => {
-			console.log(res)
-		})
-	});
+	render(<App/>, document.getElementById('root'))
+});
 
-	document.getElementById('btnFetch').addEventListener('click', function() {
-		let data = {
-			title: "DEEEEEEE", 
-			order: 10, 
-			isHidden: false
-		};
-		let config = {
-			data,
-			headers: {
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Origin': '*',
-				'Access-Control-Allow-Methods': 'GET',
-				'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
-			}
-		};
+
+
+// import axios from 'axios'
+
+// const API_URL = 'https://lviv-it-yangproject-api.herokuapp.com';
+// const LOCAL_URL = 'http://localhost:3000';
+
+// Meteor.startup(() => {
+// 	document.getElementById('btn').addEventListener('click', function() {
+// 		axios.get(API_URL + '/api/branches').then(res => {
+// 			console.log(res)
+// 		})
+// 	});
+
+// 	document.getElementById('btnFetch').addEventListener('click', function() {
+// 		let data = {
+// 			title: "DEEEEEEE", 
+// 			order: 10, 
+// 			isHidden: false
+// 		};
+// 		let config = {
+// 			data,
+// 			headers: {
+// 				'Content-Type': 'application/json',
+// 				'Access-Control-Allow-Origin': '*',
+// 				'Access-Control-Allow-Methods': 'GET',
+// 				'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
+// 			}
+// 		};
 		
-		axios.get(API_URL + '/api/branches/addNewBranch', config)
-		.then(res => console.log('the response is', res))
-		.catch((error) => console.log('the error is in', error))
+// 		axios.get(API_URL + '/api/branches/addNewBranch', config)
+// 		.then(res => console.log('the response is', res))
+// 		.catch((error) => console.log('the error is in', error))
 		
-	})
-})
+// 	})
+// })
